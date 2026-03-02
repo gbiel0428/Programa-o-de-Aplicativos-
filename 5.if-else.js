5ffb652522787cc5fc0283d9974c6fa7a9aa1b0c
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-03-02 16:51:41 -0300

    salva

---
+++ 5.if-else.js
@@ -0,0 +1,11 @@
+//  Estruturas condicionais.
+
+const idade = 20
+
+if (idade >= 18) {
+    console.log("Maior de idade")
+}else if (idade >= 11)  {
+    console.log("Adolescente")
+} else {
+    console.log("Criança")
+}
