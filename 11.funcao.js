73ee484b7764b3164c6d6703fe98115c906e99bf
gbiel0428 <aluno.den@senaibahia.com.br>
2026-03-05 16:38:58 -0300

    novo

---
+++ 11.funcao.js
@@ -0,0 +1,29 @@
+// Função comun no JavaScript
+
+function somar(a,b) {
+    return a + b
+}
+
+function subtrair(a,b) {
+    return a - b 
+}
+
+function multiplicacao(a,b) {
+    return a * b
+}
+
+function Divisao(a,b) {
+    return a / b
+}
+
+const soma = somar(2,3)
+const subtracao = subtrair(2,3)
+const multiplica = multiplicacao(2,3)
+const dividi = Divisao(10 , 10)
+
+// console.log(`Soma: ${soma}`)
+
+console.log("SOMA: ", soma)
+console.log("Subtração: ", subtracao)
+console.log("Multiplicação: ", multiplica)
+console.log("Divisão: ", dividi)
---
+++ 9.Operadores-I.js
@@ -0,0 +1,21 @@
+// Operadores lógicos.
+
+let idade = 10 
+let peso = "10"
+
+console.log(" - COMPARAÇÃO --")
+console.log("- Comparando conteúdo:")
+console.log(idade == peso)
+
+console.log("\nComparação tipo  e conteudo:")
+console.log("\nidade e peso são iguais?")
+console.log(idade === peso)
+
+console.log(" - DIFERENÇA --")
+console.log("\nComparando conteudo:")
+console.log("\nidade e peso são diferentes?")
+console.log(idade != peso)
+
+console.log("\nComparação tipo  e conteudo:")
+console.log("\nidade e peso são diferentes?")
+console.log(idade !== peso)
