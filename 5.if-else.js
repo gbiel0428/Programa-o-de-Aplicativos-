bfea8d161f4b032f9c60128891201fb7c48017c0
gbiel0428 <aluno.den@senaibahia.com.br>
2026-03-05 14:43:43 -0300

    salva

--- 5.if-else.js
+++ 5.if-else.js
@@ -6,6 +6,8 @@ if (idade >= 18) {
     console.log("Maior de idade")
 }else if (idade >= 11)  {
     console.log("Adolescente")
-} else {
+} else if (idade >=2) {
     console.log("Criança")
+} else {
+    console.log("Bebe")
 }
