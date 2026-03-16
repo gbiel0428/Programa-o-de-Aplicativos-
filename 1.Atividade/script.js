8559ff669ea3e0df4312b5bce0f02edcdb59d9f1
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-03-16 15:40:10 -0300

    atividade

--- 1.Atividade/script.js
+++ 1.Atividade/script.js
@@ -1,29 +1,23 @@
 function gerarTabuada() {
-    // Obtendo o número do input no HTML.
-    // Documento = HTML - ID = VARIAVEL NO HTML
-    const numerpInput = document.getElementById("numeroInput") ;
-    let numero = parent(numerpInput.value) ; 
-
-    //  Elemento do HTML onde o resultado será exibido.
-    const resultadoDiv = document.getElementById("resultadoTabuada") ; 
-    resultadoDiv.innerHTML = "" ;
-
-    //  Verificar  se a entrada  é um número válido.
-     if (isNaN(numero) || numero === numero || numero === "") {
-        resultadoDiv.innerHTML = "<p>Por favor, digite um número  válido.<p>"
+    // Obtendo o número do input.
+    const numeroInput = document.getElementById("numeroInput");
+    let numero = parseInt(numeroInput.value);
+    // Elemento do  HTML onde o resultado será exibido.
+    const resultadoDiv = document.getElementById("resultadoTabuada");
+    resultadoDiv.innerHTML = "";
+    // Verificar se a entrada e um número válido.
+    if (isNaN(numero)) {
+        resultadoDiv.innerHTML = "<p>Por favor, digite um número válido.</p>";
         return; // Este comando sai da função.
-     }
-
-    //  Adicionar titulo para a tabuada.
-    resultadoDiv.innerHTML += `<h2>Tabuada do número  ${numero}<h2>`
-
-    // Laço de repetição para gerar tabuada.
-    for (let i = 1; 1 <= 10;  i++) {
-        let resultado = numero * i;
-        resultadoDiv.innerHTML += `<p>${numero}  x ${i} = ${resultado}</p>` ;
     }
+    // Adicionar
+    resultadoDiv.innerHTML += `<h2>Tabuada do número ${numero}</h2>`;
 
+    for (let i = 1; i <= 10; i++) {
+        let resultado = numero * i;
+        resultadoDiv.innerHTML += `<p>${numero} x ${i} = ${resultado}</p>`;
+    }
 }
 
-const botaoGerar = document.getElementById("btnGerar");
-botaoGerar.addEventListener("click", gerarTabuada); 
+const botaoGerar = document.getElementById("BtnGerar");
+botaoGerar.addEventListener("click", gerarTabuada);
