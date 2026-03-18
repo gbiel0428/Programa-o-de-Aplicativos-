bc695ecd9df791f072a31fc92b930e279dc3c67b
Gabriel Oliveira <gabriiiel2807@gmail.com>
2026-03-18 18:25:31 -0300

    salvo

--- 2.atividade/script.js
+++ 2.atividade/script.js
@@ -36,17 +36,35 @@ produtogerar.addEventListener("click", produto );
 
 
 function menor () {
-    let numero1 = parseFloat(document.getElementById("input1").value);
-    let numero2 = parseFloat(document.getElementById("input2").value);
+    let  numero111 = document.getElementById("input1").value;
+    let   numero222 = document.getElementById("input2").value;
 
-    let rminino = numeromin =  Math.min(numero1,numero2)
-    let rmaximo = numeromax =  Math.max(numero2, numero1)
-
-    document.getElementById("Resultadocalculo").innerHTML = 
-        "Menor número: " + rminimo + " Maior número: " + rmaximo;
-    
-
-    const btnMenor = document.getElementById("calcula3");
-    btnMenor.addEventListener("click", menor);
-
+    numero111 = parseFloat(numero111);
+    numero222 = parseFloat(numero222);
+    document.getElementById("Resultadocalculo").innerHTML = resultado = Math.min(numero111, numero222);
 }
+const menorvalor = document.getElementById("calcula4")
+menorvalor.addEventListener("click", menor)
+
+function maior () {
+    let numero11 = document.getElementById("input1").value;
+    let numero22 = document.getElementById("input2").value;
+
+    numero11 = parseFloat(numero11)
+    numero22 = parseFloat(numero22)
+    document.getElementById("Resultadocalculo").innerHTML = resultado = Math.max(numero11, numero22)
+}
+
+const maiorvalor = document.getElementById("calcula3")
+maiorvalor.addEventListener("click", maior)
+
+function media () {
+    let numero12= document.getElementById("input1").value;
+    let numero21 = document.getElementById("input2").value;
+
+    numero12 = parseFloat(numero12)
+    numero21 = parseFloat(numero21)
+
+    document.getElementById("Resultadocalculo").innerHTML = resultado = (numero12 , numero21/ 2)}
+const meedia = document.getElementById("media")
+meedia.addEventListener("click", media)
