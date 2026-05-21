8a428d2713a26327bb0069c9093e0e7d6b70af43
adalberto Fernandes <adalberto.fernandes@ba.estudante.senai.br>
2026-05-21 16:48:08 -0300

    CLIENTES E FUNCIONARIOS

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
+/*
+
+//  Crie uma API COM DOIS ENDPOINTS
+/ CLIENTES
+/ FUNCIONARIOS
+
+AMBOS DEVEM TER METODOS GET, POST
+
+*/
+
+
 // Importando as dependências necessárias
+
 const express = require('express');
 const cors = require('cors');
 const { Sequelize, DataTypes } = require('sequelize');
 
 // 1. Configurando conexão com o banco de dados
-const sequelize = new Sequelize('db_api', 'root', '', {
+const sequelize = new Sequelize('db_inova', 'root', '', {
     host: 'localhost',
     dialect: 'mysql'
 });
@@ -26,6 +38,31 @@ const Cliente = sequelize.define('Cliente', {
     }
 });
 
+const Funcionario = sequelize.define('Funcionario', {
+    nome: {
+        type:DataTypes.STRING,
+        allowNull: false
+    },
+    telefone: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    cargo: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    setor: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+
+})
+
 // 3. Configuração do servidor Express
 const app = express();
 app.use(cors());
@@ -43,6 +80,16 @@ app.get('/clientes', async (req, res) => {
     }
 });
 
+app.get('/funcionarios', async(req, res) => {
+    try {
+        const funcionarios = await Funcionario.findAll();
+        res.json(funcionarios);
+    } catch (error) {
+        res.status(500).json({error: 'Error ao busca funcionario'});
+    }
+})
+
+
 // ROTA POST - Criar um novo cliente
 app.post('/clientes', async (req, res) => {
     try {
@@ -59,6 +106,24 @@ app.post('/clientes', async (req, res) => {
         res.status(500).json({ error: 'Erro ao criar cliente' });
     }
 });
+
+app.post('/funcionarios',async (req, res) => { 
+    try {
+        const {nome , telefone, email , cargo , setor} = req.body;
+
+        const novoFuncionario = await Funcionario.create({
+            nome,
+            telefone,
+            email,
+            cargo,
+            setor,
+        });
+
+        res.status(201).json(novoFuncionario);
+    } catch (error) {
+        res.status(500).json ({erro : 'Error ao criar funcionario'});
+    }
+})
 
 // 4. Sincronizando banco e iniciando servidor
 sequelize.sync()
